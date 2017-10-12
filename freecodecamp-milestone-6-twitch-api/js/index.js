98871783b0a0cb8df4b7bd425dd2bd8754413c24
JheeBz <Christian_kirkeby@hotmail.com>
2017-10-13 06:06:20 +1000

    Added some finishing touches to the style and started on JS

--- freecodecamp-milestone-6-twitch-api/js/index.js
+++ freecodecamp-milestone-6-twitch-api/js/index.js
@@ -1,7 +1,9 @@
-const baseurl = 'https://wind-bow.glitch.me/';
+feather.replace();
 
-const streamers = [
-  "ESL_SC2", 
+// axios.defaults.baseUrl = 'https://wind-bow.glitch.me/twitch-api';
+baseUrl = 'https://wind-bow.glitch.me/twitch-api';
+const streamersToDisplay = [
+  "esl_sc2", 
   "OgamingSC2",
   "cretetion",
   "freecodecamp",
@@ -11,14 +13,58 @@ const streamers = [
   "noobs2ninjas"
 ];
 
-const getStreamerData = () => {
-  endpoint = ''; 
-  $.ajax({
-    url: baseurl + endpoint,
-    dataType: 'jsonp',
-    type: 'get',
-    success: res => {
-      
-    }
-  });
+const getStreamers = (streamers) => {
+  let streamerData = [];
+  for (let i = 0; i < streamers.length; i++) {
+    $.getJSON(`${baseUrl}/streams/${streamers[i]}?callback=?`, (response) => {
+      // console.log(response);
+      if (response["stream"] === null) {
+        streamerData.push({
+          name: streamers[i],
+          status: 'Offline',
+          game: null,
+          image: null
+        });
+        console.log(streamerData);
+      } else {
+        streamerData.push({
+          name: streamers[i],
+          status: 'Online',
+          game: response.stream.game,
+          image: response.stream.channel.logo
+        });
+      }
+    });
+  }
+  return streamerData;
 }
+
+const renderList = (items) => {
+  //console.log('Inside function, outside loop');
+  // console.log(items.length);
+  let html = '';
+  for(item in items) {
+    // console.log('Inside loop');
+    html += '<li class="list-group-item streamer">';
+    let image = 'https://via.placeholder.com/50x50';
+    if (item.image === null) {
+      image = item.image;
+    }
+    html += '<img src="' + image + '" class="img-responsive rounded-circle streamer-image"></img>';
+    html += '<span class="streamer-name">' + item.name + '</span>';
+    if (item.status = 'Online') {
+      html += '<span class="badge badge-success badge-pill status">Online</span>';
+    } else {
+      html += '<span class="badge badge-secondary badge-pill status">Offline</span>';
+    }
+    if (item.game !== null) {
+      html += '<small class="text-secondary streamer-game">' + item.game + '</small>';
+    }
+    html += '</li>';
+  };
+  $('#streamers').html(html);
+}
+  
+const streamerData = getStreamers(streamersToDisplay);
+console.log(JSON.stringify(streamerData));
+renderList(streamerData);
